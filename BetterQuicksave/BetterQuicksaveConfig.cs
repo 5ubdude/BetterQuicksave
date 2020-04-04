371a01df4cf2756d04ab5b60bc18ed35b33bd121
5ubdude <63168307+5ubdude@users.noreply.github.com>
2020-04-04 16:41:40 -0500

    Add files via upload

---
+++ BetterQuicksave/BetterQuicksaveConfig.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml.Serialization;
+
+namespace BetterQuicksave
+{
+    public class BetterQuicksaveConfig
+    {
+        [XmlIgnore]
+        public string QuicksavePrefix => "quicksave_";
+        [XmlIgnore]
+        public string QuicksaveNamePattern => $@"^{QuicksavePrefix}(\d{{3}})$";
+        public int MaxQuicksaves { get; set; } = 3;
+
+        private static string ConfigFilename => 
+            $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}/../../ModuleData/BetterQuicksaveConfig.xml";
+
+        public static BetterQuicksaveConfig LoadConfig()
+        {
+            var betterQuicksaveConfig = DeserializeConfig<BetterQuicksaveConfig>(ConfigFilename);
+            if (betterQuicksaveConfig == null)
+            {
+                SerializeConfig(ConfigFilename, new BetterQuicksaveConfig());
+            }
+            return DeserializeConfig<BetterQuicksaveConfig>(ConfigFilename);
+        }
+
+        private static void SerializeConfig<T>(string fileName, T config)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(config.GetType());
+            using (StreamWriter streamWriter = File.CreateText(fileName))
+            {
+                xmlSerializer.Serialize(streamWriter, config);
+            }
+        }
+
+        private static T DeserializeConfig<T>(string fileName) where T : class
+        {
+            T result;
+            if (!File.Exists(fileName))
+            {
+                result = default(T);
+            }
+            else
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                try
+                {
+                    using (StreamReader streamReader = File.OpenText(fileName))
+                    {
+                        result = (T)((object)xmlSerializer.Deserialize(streamReader));
+                    }
+                }
+                catch (Exception)
+                {
+                    if (File.Exists(fileName))
+                    {
+                        File.Delete(fileName);
+                    }
+                    result = default(T);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ BetterQuicksave/Patches.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using TaleWorlds.Core;
+using HarmonyLib;
+
+namespace BetterQuicksave
+{
+    public static class Patches
+    {
+        [HarmonyPatch(typeof(MBSaveLoad), "QuickSaveCurrentGame")]
+        public class QuickSaveCurrentGamePatch
+        {
+            static readonly MethodInfo OverwriteSaveFile = AccessTools.Method(typeof(MBSaveLoad), "OverwriteSaveFile");
+
+            static readonly MethodInfo GetQuicksaveName =
+                SymbolExtensions.GetMethodInfo(() => QuicksaveManager.GetQuicksaveName());
+
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                foreach (CodeInstruction instruction in instructions)
+                {
+                    if (instruction.opcode == OpCodes.Call && instruction.operand == OverwriteSaveFile)
+                    {
+                        yield return new CodeInstruction(OpCodes.Pop);
+                        yield return new CodeInstruction(OpCodes.Call, GetQuicksaveName);
+                    }
+
+                    yield return instruction;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(MBSaveLoad), "IsSaveFileNameReserved")]
+        public class IsSaveFileNameReservedPatch
+        {
+            static void Postfix(ref bool __result, string name)
+            {
+                __result = QuicksaveManager.IsValidName(name) || __result;
+            }
+        }
+    }
+}
