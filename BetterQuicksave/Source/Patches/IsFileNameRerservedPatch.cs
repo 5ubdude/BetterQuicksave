ae39edc3e0ff41814a426dce58cfb302c348ce0d
5ubdude <63168307+5ubdude@users.noreply.github.com>
2020-04-07 11:00:24 -0500

    refactor, fix SubModule.xml, added load/error messages (#2)
    * refactor, fix SubModule.xml, added load/error messages
    
    * remove unused reference

---
+++ BetterQuicksave/Source/Patches/IsFileNameRerservedPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+
+namespace BetterQuicksave.Patches
+{
+    [HarmonyPatch(typeof(MBSaveLoad), "IsSaveFileNameReserved")]
+    public class IsSaveFileNameReservedPatch
+    {
+        static void Postfix(ref bool __result, string name)
+        {
+            __result = QuicksaveManager.IsValidName(name) || __result;
+        }
+    }
+}
